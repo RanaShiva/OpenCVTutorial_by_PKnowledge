7f1dc09c400df57552efb45b7dc26d58a1ee9ca2
RanaShiva <shivamrana.4wrk@gmail.com>
2023-01-12 14:11:30 +0530

    Background subtraction using openCV code added

---
+++ opencv_background_subtraction.py
@@ -0,0 +1,18 @@
+import numpy as np
+import cv2 as cv
+cap = cv.VideoCapture('C:/Users/evig0/OneDrive/Desktop/research/opencv-4.x/samples/data/vtest.avi')
+fgbg = cv.createBackgroundSubtractorKNN(detectShadows=False)
+while True:
+    ret, frame = cap.read()
+    if frame is None:
+        break
+    fgmask = fgbg.apply(frame)
+    cv.imshow('Frame', frame)
+    cv.imshow('FG MASK Frame', fgmask)
+
+    keyboard = cv.waitKey(30)
+    if keyboard == 'q' or keyboard == 27:
+        break
+
+cap.release()
+cv.destroyAllWindows()
